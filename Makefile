ec6abf30430e4a293b0ec2d06be915cae54e20c1
KevinFalah <kevin.ghossan@mandalafinance.com>
2024-12-01 14:30:49 +0700

    finished airdrop

--- Makefile
+++ Makefile
@@ -52,7 +52,8 @@ anvil :; anvil -m 'test test test test test test test test test test test junk' 
 
 zk-anvil :; npx zksync-cli dev start
 
-NETWORK_ARGS := --rpc-url http://localhost:8545 --private-key $(DEFAULT_ANVIL_KEY) --broadcast
+# NETWORK_ARGS := --rpc-url http://localhost:8545 --private-key $(DEFAULT_ANVIL_KEY) --broadcast
+NETWORK_ARGS := --rpc-url $(SEPOLIA_RPC_URL) --broadcast
 
 ifeq ($(findstring --network sepolia,$(ARGS)),--network sepolia)
 	NETWORK_ARGS := --rpc-url $(SEPOLIA_RPC_URL) --private-key $(PRIVATE_KEY) --broadcast --verify --etherscan-api-key $(ETHERSCAN_API_KEY) -vvvv
--- script/Interact.s.sol
+++ script/Interact.s.sol
@@ -1,16 +1,15 @@
 pragma solidity ^0.8.24;
 
-import {Script} from "forge-std/Script.sol";
-import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
 
 contract ClaimAirdrop is Script {
-    address constant CLAIMING_ADDRESS = 0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D;
+    address constant CLAIMING_ADDRESS = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     uint256 constant CLAIMING_AMOUNT = 25 * 1e18;
-    bytes32 constant PROOF_1 = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 constant PROOF_1 = 0xa24ade2ae6f52f82ecae64070c08ae4a8e1fca2aeb44f468081ed6129559e7a8;
     bytes32 constant PROOF_2 = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] proof = [PROOF_1, PROOF_2];
-    bytes private signature = hex"5552b3941020c6c176ad02dbbba1769ad7e42cfd354052a0f9b90daabb514880471dc7dac296e281577c52a1d1f861ae2b2ffee63ed8c05b1e361f2305dda2221c";
+    bytes private signature = hex"cbd08ea4910eeb88f0fc9926bcccdc9400f17caffb6ce694e64ba7208af7fb35067e203a1e306fb860f43f36ac82c883362f86c5aff3a0c6aa1207baaec28d081b";
 
     error ClaimAirdropScript__InvalidSignature();
 
@@ -34,10 +33,11 @@ contract ClaimAirdrop is Script {
     }
 
     function run() external {
-        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
-            "MerkleAirdrop",
-            block.chainid
-        );
+        // address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+        //     "MerkleAirdrop",
+        //     block.chainid
+        // );
+        address mostRecentDeployed = 0x19386214eE713B4A1609df4035085dDc829c4D1C;
 
         claimAirdrop(mostRecentDeployed);
     }
